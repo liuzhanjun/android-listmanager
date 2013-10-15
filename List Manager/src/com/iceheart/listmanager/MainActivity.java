949bce5a3f99a9fbbd908cf9fa34deaa4cceb560
nmasse68 <nmasse@gmail.com>
2013-10-15 14:46:19 -0400

    Adjusted price box (may still become an image); Fixed a crash while loading the list of tasks initially (i.e. when no list exists)

--- List Manager/src/com/iceheart/listmanager/MainActivity.java
+++ List Manager/src/com/iceheart/listmanager/MainActivity.java
@@ -431,14 +431,15 @@ public class MainActivity extends Activity  {
 			buffer.append( "All Tasks" );
 		}
 		buffer.append( "\n\n" );
-		
-		for ( Task task: tasks ) {
-			if ( buffer.length() > 0 ) {
-				buffer.append( "\n" );
-			}
-			buffer.append( "- ");
-			buffer.append( task.getName() );
-		}
+		if ( tasks != null ) {
+            for ( Task task: tasks ) {
+                if ( buffer.length() > 0 ) {
+                    buffer.append( "\n" );
+                }
+                buffer.append( "- ");
+                buffer.append( task.getName() );
+            }
+        }
 		return buffer.toString();
 		
 	}
